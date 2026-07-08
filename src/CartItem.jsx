e04a5097f6b1f3416ed912b1a01c393d85386e53
MaFa28 <marcomorales.mm41@gmail.com>
2026-07-08 12:56:57 -0400

    corrigiendo sintaxis

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+// Se importó addItem para cumplir con las instrucciones
+import { removeItem, updateQuantity, addItem } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
